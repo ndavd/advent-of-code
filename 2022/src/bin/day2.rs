3d33fc4ee84fb995e4e16500b1cb3a6ef2332937
Nuno David <ndavidq0@gmail.com>
2022-12-03 01:14:14 +0000

    Feat: Day 2

---
+++ 2022/src/bin/day2.rs
@@ -0,0 +1,85 @@
+use aoc::*;
+
+#[derive(Clone, Copy)]
+enum GameMove {
+    Rock = 1,
+    Paper,
+    Scissors,
+}
+
+#[derive(Clone, Copy)]
+enum GameResult {
+    Win = 6,
+    Lose = 0,
+    Draw = 3,
+}
+
+fn get_game_result(player_a_move: GameMove, player_b_move: GameMove) -> GameResult {
+    match (player_a_move, player_b_move) {
+        (GameMove::Rock, GameMove::Scissors)
+        | (GameMove::Paper, GameMove::Rock)
+        | (GameMove::Scissors, GameMove::Paper) => GameResult::Win,
+        (GameMove::Rock, GameMove::Rock)
+        | (GameMove::Paper, GameMove::Paper)
+        | (GameMove::Scissors, GameMove::Scissors) => GameResult::Draw,
+        _ => GameResult::Lose,
+    }
+}
+
+fn get_game_move(game_result: GameResult, player_b_move: GameMove) -> GameMove {
+    match (game_result, player_b_move) {
+        (GameResult::Draw, GameMove::Rock)
+        | (GameResult::Win, GameMove::Scissors)
+        | (GameResult::Lose, GameMove::Paper) => GameMove::Rock,
+        (GameResult::Draw, GameMove::Paper)
+        | (GameResult::Win, GameMove::Rock)
+        | (GameResult::Lose, GameMove::Scissors) => GameMove::Paper,
+        _ => GameMove::Scissors,
+    }
+}
+
+pub fn get_answer(input: Vec<String>) -> (i32, i32) {
+    let mut points_won = 0;
+    let mut real_points_won = 0;
+
+    input.iter().for_each(|prediction| {
+        let mut iter = prediction.split_whitespace().into_iter();
+
+        let opponent_move = match iter.next().unwrap() {
+            "A" => GameMove::Rock,
+            "B" => GameMove::Paper,
+            "C" => GameMove::Scissors,
+            _ => panic!("Invalid move from opponent!"),
+        };
+
+        let guess = iter.next().unwrap();
+        let my_move = match guess {
+            "X" => GameMove::Rock,
+            "Y" => GameMove::Paper,
+            "Z" => GameMove::Scissors,
+            _ => panic!("Invalid move from me!"),
+        };
+
+        points_won += my_move as i32 + get_game_result(my_move, opponent_move) as i32;
+
+        let game_result_prediction = match guess {
+            "X" => GameResult::Lose,
+            "Y" => GameResult::Draw,
+            "Z" => GameResult::Win,
+            _ => panic!("Invalid game result!"),
+        };
+        let my_real_move = get_game_move(game_result_prediction, opponent_move);
+        real_points_won += my_real_move as i32 + game_result_prediction as i32;
+    });
+
+    (points_won, real_points_won)
+}
+
+fn main() -> Result<(), ()> {
+    let day = 2;
+    let test_input = get_input(day, true)?;
+    assert_eq!(get_answer(test_input), (15, 12));
+
+    let input = get_input(day, false)?;
+    output_answer(get_answer(input), day)
+}
